ffba2f35fcd64d26c4d27bcde0e0e7c4074071e5
debora <deborasouza.p@outlook.com>
2025-07-16 11:37:44 -0300

    corrigindo caminho

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,6 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  const img = document.querySelector("#profile img")
   const icon = document.querySelector("#switch button img")
 
   if (html.classList.contains("light")) {
